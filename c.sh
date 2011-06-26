4e2ab6b4a0f868ddeffe6fb2d03cfc64b97044f4
Miguel Serrano <miguelvps@gmail.com>
2011-06-26 21:16:56 +0100

    Check for readline completion-ignore-case

--- c.sh
+++ c.sh
@@ -3,7 +3,9 @@
 function c() {
     cd "`command c $@`"
 }
-# if readline completion-ignore-case is set to `on'
-complete -o filenames -C "command c -ci" c
-# else
-# complete -o filenames -C "command c -c" c
+if [[ `bind -v` == *"set completion-ignore-case on"* ]]
+then
+    complete -o filenames -C "command c -ci" c
+else
+    complete -o filenames -C "command c -c" c
+fi
