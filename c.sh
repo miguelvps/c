303996e5a3e55fbc6b09ffa6c981494352a62152
Miguel Serrano <miguelvps@gmail.com>
2011-07-01 21:34:56 +0100

    Allow options to be defined in an environment var

--- c.sh
+++ c.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function c() {
-    cd "`command c $@`"
+    cd "`command c $_C_OPTS $@`"
 }
 if [[ `bind -v` == *"set completion-ignore-case on"* ]]
 then
-    complete -o filenames -C "command c -ci" c
+    complete -o filenames -C "command c \$_C_OPTS -ci" c
 else
-    complete -o filenames -C "command c -c" c
+    complete -o filenames -C "command c \$_C_OPTS -c" c
 fi
